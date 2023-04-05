99a9d3bc72b27f91a860882d84a78123de91b859
tbachvarova <teodora.sivkova@gmail.com>
2023-04-05 16:09:37 +0300

    var.iddigits

--- main.py
+++ main.py
@@ -2,13 +2,19 @@ calculation_unit = 24
 unit_name = "hours"
 
 def daysToUnits(numOfDays):
-    if(numOfDays > 0):
+    if numOfDays > 0:
         print(f"{numOfDays} days are {numOfDays * calculation_unit} {unit_name}")
+    elif numOfDays == 0:
+        print("It must be grater the 0")
     else:
         print("Number must be Grater than 0")
 
+
 userInput = input("Add some number for days: ")
 
-daysToUnits(int(userInput))
+if userInput.isdigit():
+    daysToUnits(int(userInput))
+else:
+    print("Try again with number!")
 
 # print(type(1))
