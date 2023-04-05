3cf41dfa5f586e9e364001fa48214ce2f60e60f6
tbachvarova <teodora.sivkova@gmail.com>
2023-04-05 17:02:15 +0300

    some data types

--- main.py
+++ main.py
@@ -1,26 +1,41 @@
 calculation_unit = 24
 unit_name = "hours"
 
+
 def daysToUnits(numOfDays):
-    conditionCheck = numOfDays > 0
-    print(type(conditionCheck))
-    
-    if conditionCheck:
-        print(f"{numOfDays} days are {numOfDays * calculation_unit} {unit_name}")
-    elif numOfDays == 0:
-        print("It must be grater the 0")
+   return f"{numOfDays} days are {numOfDays * calculation_unit} {unit_name}"
+  #  print(type(conditionCheck))
+
+
+
+
+
+
+def validationAndExecute():
+
+    if userInput.isdigit():
+        userInputNum = int(userInput)
+
+        if userInputNum > 0:
+           calcValue = daysToUnits(userInputNum)
+           print(calcValue)
+
+        elif userInputNum == 0:
+            print("It must be grater the 0")
+
+        else:
+            print("Number must be Grater than 0")
+
+
     else:
-        print("Number must be Grater than 0")
+        print("Try again with number!")
 
 
-userInput = input("Add some number for days: ")
-
-if userInput.isdigit():
-    daysToUnits(int(userInput))
-else:
-    print("Try again with number!")
 
 print(type(1))
 print(type(33.99))
 print(type("alabala"))
-print(type(9>0))
+print(type(9 > 0))
+
+userInput = input("Add some number for days: ")
+validationAndExecute()
