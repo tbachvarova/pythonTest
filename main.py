d7d205e0707eb2b395ae9cd5a49c8dcdf5a7bd8d
tbachvarova <teodora.sivkova@gmail.com>
2023-04-05 15:26:46 +0300

    user INPUT + calc

--- main.py
+++ main.py
@@ -1,15 +1,12 @@
 calculation_unit = 24
 unit_name = "hours"
 
-def daysToUnits(numOfDays, customMessage):
+def daysToUnits(numOfDays):
     print(f"{numOfDays} days are {numOfDays * calculation_unit} {unit_name}")
-    print(customMessage)
 
 
-daysToUnits(20, "Super!")
-daysToUnits(35, "Super custom :)")
-
-userInput = input("Add something: ")
+userInput = input("Add some number for days: ")
 userInputNum = int(userInput)
 
-print(f"User added: {userInputNum}")
+print(f"User added: {userInputNum} days")
+daysToUnits(userInputNum)
